fbf03e705883fd9087f6d6541e2a9b5fe9832faf
Manu Elementary Os 2020 <realgarcilaso@outlook.com.pe>
2020-11-17 06:33:25 -0500

    3.b Creando question factory
    creando 3 users.
    y preguntas al azar para cada user.

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(App\Question::class, function (Faker $faker) {
+    return [
+
+        #---- el ".": elimina el punto q se le da a la frase creada.
+        'title' => rtrim($faker->sentence(rand(5, 10)), "."),
+        #---- el 3, significa el 3 saltos de linea
+        'body' => $faker->paragraphs(rand(3, 7), true),
+        'views' => rand(0, 10),
+        'answers' => rand(0, 10),
+        'votes' => rand(-3, 10)
+
+
+    ];
+});
