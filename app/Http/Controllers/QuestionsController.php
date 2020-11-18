e95388c974a726816bf3348e1041ab0360f5594b
Manu Elementary Os 2020 <realgarcilaso@outlook.com.pe>
2020-11-18 07:06:30 -0500

    6.a Question: adding square of votes, answers and view counters
    por el momento es estatico.

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -15,10 +15,9 @@ class QuestionsController extends Controller
 
     public function index()
     {
-        \DB::enableQueryLog();
-        $questions = Question::with('user')->latest()->paginate(10);
-        view('questions.index', compact('questions'))->render();
-        dd(\DB::getQueryLog());
+        $questions = Question::with('user')->latest()->paginate(5);
+        return view('questions.index', compact('questions'));
+
     }
 
     /**
