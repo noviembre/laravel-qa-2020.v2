1946c3201c58ed7327f327bc490c8004b71bfe97
Manu Elementary Os 2020 <realgarcilaso@outlook.com.pe>
2020-11-23 08:34:55 -0500

    12B.b aplicandolo en los metodos
    de update y delete.

--- app/Policies/QuestionPolicy.php
+++ app/Policies/QuestionPolicy.php
@@ -20,7 +20,7 @@ class QuestionPolicy
      */
     public function update(User $user, question $question)
     {
-
+        return $user->id === $question->user_id;
     }
 
     /**
@@ -32,6 +32,10 @@ class QuestionPolicy
      */
     public function delete(User $user, question $question)
     {
-        
+        #----- the question that has 1 or more answers cant not be removed even by the creator
+        #---- if the current user is he creator of the question, and the question that will be removed
+        #---- has no answer then the creator of the answer will be authorize to remove
+
+        return $user->id === $question->user_id && $question->answers < 1;
     }
 }
