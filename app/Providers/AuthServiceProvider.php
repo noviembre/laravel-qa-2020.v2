afebf3867274d7d7e1d317584bf0fba2c52e81d3
Manu Elementary Os 2020 <realgarcilaso@outlook.com.pe>
2020-11-23 07:09:05 -0500

    12A.a. Gates: Creando el metodo.
    verificando si la pregunta actual pertenece al usuario actual.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,5 +26,20 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
+        # the method define(), accept 2 arguments; the first one is a name "update-question",
+        # the second one is a closers that contains 2 arg...
+        #(1st is a user instance, and this instance represence the current user sign ir;
+        # and the 2nd is the model instance that will authorize)
+        \Gate::define('update-question', function($user, $question) {
+            # this returns a bolean value, true if both are catch or  false if they not match
+            return $user->id === $question->user_id;
+        });
+
+        \Gate::define('delete-question', function($user, $question) {
+            return $user->id === $question->user_id;
+        });
+
+        # se puede usar el mismo Gate tanto para update como para delete pero asumamos que este usuario
+        # sera dif en el futuro. asi que lo dejamos como esta.
     }
 }
