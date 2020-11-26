2128f8fe0057b3a53de9bb11879311da52664ebd
Manu Elementary Os 2020 <realgarcilaso@outlook.com.pe>
2020-11-26 06:01:00 -0500

    16.e Vote Controls every Answers
    en questions details obviusly

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -31,7 +31,7 @@
                                     <i class="fas fa-caret-down fa-3x"></i>
                                 </a>
                                 <a title="Click to mark as favorite question (Click again to undo)" class="favorite">
-                                    
+
                                     <i class="fas fa-star fa-2x"></i>
 
                                     <span class="favorites-count">123</span>
@@ -70,6 +70,20 @@
                         <hr>
                         @foreach ($question->answers as $answer)
                             <div class="media">
+
+                                <div class="d-fex flex-column vote-controls">
+                                    <a title="This answer is useful" class="vote-up">
+                                        <i class="fas fa-caret-up fa-3x"></i>
+                                    </a>
+                                    <span class="votes-count">1230</span>
+                                    <a title="This answer is not useful" class="vote-down off">
+                                        <i class="fas fa-caret-down fa-3x"></i>
+                                    </a>
+                                    <a title="Mark this answer as best answer" class="vote-accepted mt-2">
+                                        <i class="fas fa-check fa-2x"></i>
+                                    </a>
+                                </div>
+                                
                                 <div class="media-body">
                                     {!! $answer->body_html !!}
                                     <div class="float-right">
