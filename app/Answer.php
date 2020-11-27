c2ee7a256e11d9e9a461bf8884fcbbec13312196
Manu Elementary Os 2020 <realgarcilaso@outlook.com.pe>
2020-11-27 07:08:22 -0500

    19-A-1way Step 1
    para realizar de esta forma debemos seguir 2 pasos
    el primero esta dentro de este commit.
    y el otro en el siguiente commit, donde se indica todo que se hace en tinker.

--- app/Answer.php
+++ app/Answer.php
@@ -30,13 +30,26 @@ class Answer extends Model
         static::created(function ($answer) {
             $answer->question->increment('answers_count');
         });
-        
+
         #--- when a new answer is deleted the counter will be decrement
         static::deleted(function ($answer) {
-            $answer->question->decrement('answers_count');
+
+            $question = $answer->question;
+            $question->decrement('answers_count');
+            if ($question->best_answer_id === $answer->id) {
+                $question->best_answer_id = NULL;
+                $question->save();
+            }
+
         });
 
     }
+
+    public function getStatusAttribute()
+    {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
+    }
+    
     public function getCreatedDateAttribute()
     {
         return $this->created_at->diffForHumans();
